cef3a50264fb42264c6c87eb45ab277511f4a8f0
mquiros@innventto.com <mquiros@innventto.com>
2016-03-12 11:19:13 -0500

    Initial Commit

---
+++ alphaScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alphaScreen
+//
+//  Created by mkdev on 12/03/16.
+//  Copyright © 2016 Monitron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alphaScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alphaScreen
+//
+//  Created by mkdev on 12/03/16.
+//  Copyright © 2016 Monitron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
